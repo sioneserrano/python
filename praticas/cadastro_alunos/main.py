def315217c25a19d5970a9470c444e8c182040e8
sione.serrano <thisissione1204@gmail.com>
2026-08-15 21:04:27 -0300

    Terminei a pratica usando funcoes

--- praticas/cadastro_alunos/main.py
+++ praticas/cadastro_alunos/main.py
@@ -1,85 +1,23 @@
-alunos = []
+from funcoes import * 
 
 while True:
-
-    print("1-Cadastrar aluno")
-    print("2-Listar alunos")
-    print("3-Cálcular média")
-    print("4-Mostrar aprovado/reprovado")
-    print("5-Sair")
-   
-    print("\n")
-    opcao = int(input("Informe uma opção: "))
+    menu()
+    opcao = int(input("Informe uma opcao: "))
     
-    print("\n")
-
     if opcao == 1:
+      cadastrar_aluno()
 
-        print("Informe os dados do aluno")
-       
-        nome = input("Nome: ")
-       
-        nota1 = float(input("Nota 1: "))
-       
-        nota2 = float(input("Nota 2: "))
-
-
-        media = (nota1 + nota2)//2
-
-        print("\n")
-       
-        aluno = {
-            "nome": nome,
-            "nota1": nota1,
-            "nota2": nota2,
-            "media": media 
-        }
-       
-        alunos.append(aluno)
-       
     elif opcao == 2:
-        print("Lista dos alunos")
-        #print(alunos)
+      listar_alunos()
 
-        i = 0
-        while i < len(alunos):
-          print(f"{i+1}- {alunos[i]["nome"]}")  #(???)
-          i=i+1
-
-        print("\n")
-             
     elif opcao == 3:
+      mostrar_media()
 
-        print("Média das provas")
-        print("")
-
-        i = 0
-        while i < len(alunos):
-          print(f"{i+1}- {alunos[i]["nome"]}")  #(???)
-          print(f"Média:{alunos[i]["media"]}\n")
-          i=i+1
-
-        print("\n")
-
-        
     elif opcao == 4:
-        
-        print("Resultados")
-        print("")
-
-        i = 0
-        while i < len(alunos):
-          print(f"{i+1}- {alunos[i]["nome"]}")  #(???)
-          print(f"Média:{alunos[i]["media"]}")
-
-          if alunos[i]["media"] >=6:
-            print("Aprovado")
-          else:
-            print("Reprovado")
-          i=i+1
-          print("\n")
+      mostrar_resultado()
 
     elif opcao == 5:
-        break
+      print("Saindo...")
+      break
     else:
-        print("Opção invalida!")
+      print("Opção inválida!\n")
