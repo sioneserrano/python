def8b3a7e62199ba20b3b78337cfd55125e9e304
sione.serrano <thisissione1204@gmail.com>
2026-08-15 21:04:27 -0300

    Terminei a pratica usando funcoes

---
+++ praticas/cadastro_alunos/funcoes.py
@@ -0,0 +1,65 @@
+
+alunos = []
+
+def menu():
+    print("1-Cadastrar aluno")
+    print("2-Listar alunos")
+    print("3-Calcular média")
+    print("4-Mostrar aprovado/reprovado")
+    print("5-Sair")
+    print("\n")
+
+def cadastrar_aluno():
+    print("Informe os dados do aluno")
+    nome = input("Nome: ")
+    nota1 = float(input("Nota 1: "))
+    nota2 = float(input("Nota 2: "))
+    
+    media = (nota1 + nota2) / 2
+    
+    aluno = {
+        "nome": nome,
+        "nota1": nota1,
+        "nota2": nota2,
+        "media": media 
+    }
+    alunos.append(aluno)
+    print("\nAluno cadastrado com sucesso!\n") 
+    
+def mostrar_media():
+    print("\n")
+    print("Média das provas:")
+    i = 0
+    while i < len(alunos):
+        print(f'{i+1}- {alunos[i]["nome"]}') 
+        print(f'Média: {alunos[i]["media"]:.1f}\n')  
+        i = i + 1
+    print("\n")
+
+def listar_alunos():
+    if len(alunos) == 0:
+        print("Nenhum aluno cadastrado!\n")
+    else:
+        print("\n")
+        print("Lista dos alunos:")
+        i = 0
+        while i < len(alunos):
+            print(f'{i+1}- {alunos[i]["nome"]}')
+            i = i + 1
+        print("\n")
+
+def mostrar_resultado():
+    print("\n")
+    print("Resultados:")
+    i = 0
+    while i < len(alunos):
+        print(f'{i+1}- {alunos[i]["nome"]}') 
+        print(f'Média: {alunos[i]["media"]:.1f}')
+        
+        if alunos[i]["media"] >= 6:
+            print("Aprovado")
+        else:
+            print("Reprovado")
+        print("")
+        i = i + 1
+    print("\n")
